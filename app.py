016a1a77cdcbe76bff5666519ea0d743d43544a1
Reed Krawiec <reedkrawiec@gmail.com>
2021-01-14 21:48:08 -0500

    Removed Unnecessary data download

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.1307), (0.3081))])
 
-testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-
 import torch.nn as nn
 import torch.nn.functional as F
 
